0addb72f55ef27c657dc89f9bc485cd9ca917ac7
Seema-476 <simmybishnoi29085@gmail.com>
2024-09-12 09:16:27 +0530

    js practice

---
+++ assets/js/script.js
@@ -0,0 +1,34 @@
+
+let a = prompt("Enter a no.");
+a = Number.parseInt(a);
+
+if (a < 20) {
+    alert("Welcome!");
+} else if (a >= 20 || a <= 50) {
+    alert("Good work");
+} else {
+    alert("Thank You for this work.");
+}
+
+let year = new Date().getFullYear();
+switch (new Date().getFullYear()) {
+    case 0:
+        year = "2023";
+        break;
+    case 1:
+        year = "2024";
+        break;
+    case 2:
+        year = "2025";
+        break;
+    case 3:
+        year = "2026";
+        break;
+    case 4:
+        year = "2027";
+        break;
+    case 5:
+        year = "2028";
+        break;
+}
+console.log(year);
