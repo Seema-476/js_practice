e836ebd310c293bcffe42b635279ea3c2bd620eb
Seema-476 <simmybishnoi29085@gmail.com>
2024-09-12 21:12:13 +0530

    changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,15 @@
-
+// if else 
 let a = prompt("Enter a no.");
 a = Number.parseInt(a);
 
 if (a < 20) {
     alert("Welcome!");
-} else if (a >= 20 || a <= 50) {
+} else if (a >= 20 && a <= 50) {
     alert("Good work");
 } else {
     alert("Thank You for this work.");
 }
-
+// print year 
 let year = new Date().getFullYear();
 switch (new Date().getFullYear()) {
     case 0:
@@ -31,4 +31,14 @@ switch (new Date().getFullYear()) {
         year = "2028";
         break;
 }
+document.getElementById("year").innerHTML = "today" + new Date().getFullYear();
 console.log(year);
+
+// ternary operator
+let b = 30;
+let c = 40;
+(b>c? c="yes":"no")
+document.getElementById("demo").innerHTML = "value" +"=" +(b > c ? "" : "");
+
+
+
